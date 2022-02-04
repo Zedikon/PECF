baf58f9c0d2ed93ff67823a85b236e3d79fcbe95
Zedikon <70371915+Zedikon@users.noreply.github.com>
2022-02-04 12:55:06 +0300

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []
 
 setup(
     name="PECF",
-    version="1.0.23",
+    version="1.0.25",
     author="Zedikon",
     author_email="mrzedikon@gmail.com",
     description="New easy and accessible configuration language",
