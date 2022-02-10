e89cacbe9a0c4e1c2053e4398cedf1c422aecc29
Zedikon <70371915+Zedikon@users.noreply.github.com>
2022-02-10 10:12:13 +0300

    Update main.py

--- example/main.py
+++ example/main.py
@@ -1,5 +1,14 @@
+# for version 1.0.25
 import PECF as pecf
 
 hehe = open("main.pecf").read()
 a = pecf.decode(hehe, "hehe")
 print(a)
+
+# for version 1.0.26
+import PECF as pecf
+
+hehe = open("main.pecf").read()
+a = pecf.load(hehe)
+b = pecf.decode("hehe")
+print(b)
