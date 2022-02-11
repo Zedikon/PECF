7acaa4ed5695ed0f53ea47419dfc1f82ac4ddfc0
Zedikon <70371915+Zedikon@users.noreply.github.com>
2022-02-12 01:00:10 +0300

    Update hello_world.py

--- example/hello_world.py
+++ example/hello_world.py
@@ -4,3 +4,11 @@ import PECF as pecf
 
 main = "world: Hello, str hello: world! str"
 print(pecf.decode(main, "world") + " " + pecf.decode(main, "hello"))
+
+# Hello world on PECF 1.0.25
+
+import PECF as pecf
+
+main = "world: str: Hello hello: str: world"
+pecf.load(main)
+print(pecf.decode("world") + " " + pecf.decode("hello"))
